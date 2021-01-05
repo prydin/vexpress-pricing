d7be9a4418c8c879c64b980453d8200fab0fa945
prydin <prydin@vmware.com>
2021-01-05 11:14:48 -0500

    WIP

--- scripts/configureAppserver.sh
+++ scripts/configureAppserver.sh
@@ -4,7 +4,7 @@ mkdir /opt/vexpress-pricing
 chown $1 /opt/vexpress-pricing
 cd /opt/vexpress-pricing
 mv /tmp/application.properties .
-wget --auth-no-challenge --user=$2 --password=$3 $4/artifact/build/libs/pricing-$5.jar -O pricing.jar
+wget --auth-no-challenge --user=$2 --password=$3 $4/artifact/build/libs/vexpress-pricing-$5.jar -O vexpress-pricing.jar
 mv /tmp/vexpress-pricing.service /etc/systemd/system
 chmod 664 /etc/systemd/system/vexpress-pricing.service
 systemctl enable vexpress-pricing
